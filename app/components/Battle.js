ae786e8ec0e51f540078e29034bc436a7d933b6d
Matt Peters <matthew.george.peters@gmail.com>
2019-01-29 18:49:45 -0500

    added state and handleSubmit fx

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,6 +2,28 @@ var React = require('react');
 var Link = require('react-router-dom').Link;
 
 class Battle extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            playerOneName: '',
+            playerTwoName: '',
+            playerOneImage: null,
+            playerTwoImage: null
+        }
+
+        this.handleSubmit = handleSubmit.bind(this);
+    }
+
+    handleSubmit(id, username){
+        this.setState(function() {
+            var newState = {};
+            newState[id + 'Name'] = username;
+            newState[id + 'Image'] = 'http://github.com/' + username + '.png?size=200';
+            return newState;
+        })
+    }
+
     render() {
         return (
             <div>
