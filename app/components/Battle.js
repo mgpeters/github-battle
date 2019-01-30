b83a8c32973b27220b062130d8c211a0731000db
Matt Peters <matthew.george.peters@gmail.com>
2019-01-29 19:56:48 -0500

    built out battle.js

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,65 @@
 var React = require('react');
 var Link = require('react-router-dom').Link;
+var PropTypes = require('prop-types');
+
+class PlayerInput extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            username: ''
+        }
+        handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        var value = event.target.value;
+
+        this.setState(function() {
+            return {
+                username: value
+            }
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.props.onSubmit(
+            this.props.id,
+            this.state.username
+        )
+    }
+
+    render() {
+        return (
+            <form className='column' onSubmit={this.handleSubmit}>
+                <label className='header' htmlFor='username'>
+                    {this.props.label}
+                </label>
+                <input
+                    id='username'
+                    placeholder='github username'
+                    type='text'
+                    autoComplete='off'
+                    value={this.state.username}
+                    onChange={this.handleChange} />
+            </form>
+            <button
+                className='button'
+                type="submit"
+                disabled={!this.state.username}>
+                    Submit
+                </button>
+        )
+    }
+}
+
+PlayerInput.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired
+}
 
 class Battle extends React.Component {
     constructor(props){
@@ -12,7 +72,7 @@ class Battle extends React.Component {
             playerTwoImage: null
         }
 
-        this.handleSubmit = handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(id, username){
@@ -25,9 +85,26 @@ class Battle extends React.Component {
     }
 
     render() {
+    var playerOneName = this.state.playerOneName,
+        playerTwoName = this.state.playerTwoName;
+
         return (
             <div>
-                Battle!
+                <div className='row'>
+                    {!playerOneName &&
+                        <PlayerInput 
+                        id='playerOne'
+                        label='Player One'
+                        handleSubmit={ this.handleSubmit }
+                    />}
+
+                    {!playerTwoName &&
+                        <PlayerInput 
+                        id='playerTwo'
+                        label='Player Two'
+                        onSubmit={ this.handleSubmit }
+                        />}
+                </div>
             </div>
         )
     }
