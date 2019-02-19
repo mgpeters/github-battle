2be98b17664c4ee4776a89f1dc047aa014eea2b4
Matt Peters <matthew.george.peters@gmail.com>
2019-02-19 18:56:50 -0500

    created loading comp - defaultprops

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Loading = require('./Loading');
 var api = require('../utils/api');
 
 function SelectLanguage (props) {
@@ -96,7 +97,7 @@ class Popular extends React.Component {
                     selectedLanguage = { this.state.selectedLanguage }
                     onSelect = { this.updateLanguage }/>
                 {!this.state.repos 
-                    ? <p>Loading...</p>
+                    ? <Loading />
                     : <RepoGrid repos = { this.state.repos }
                     />}
             </div>
